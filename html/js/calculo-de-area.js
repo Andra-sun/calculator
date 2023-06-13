7d0f4ba846c59b16499c7465a65cbc4f5a799d40
wendellast <wendelalves02a@hotmail.com>
2023-06-13 14:21:49 -0300

    adicionando area de novo

--- html/js/calculo-de-area.js
+++ html/js/calculo-de-area.js
@@ -1,17 +1,30 @@
 var oqueElement = document.getElementById("oque");
 var selectList = document.getElementById("formas");
 var input = document.getElementById("formula");
-const base = document.querySelector("#ib");
-const altura = document.getElementById("item-d");
-const areaq = document.getElementById("item-a");
 
-oqueElement.addEventListener("change", function() {
-  var modo = oqueElement.value;
 
-  if (modo === "area") {
-    if (selectList.value === "quadrado") { 
-      const area = base.value * altura.value;
-      areaq.value = area;
-    }
+const base = document.getElementById("baseId");
+const altura = document.getElementById("alturaId");
+let resultado = document.getElementById('resposta2')
+
+const mode = oqueElement.value;
+const formaFigura = selectList.value;
+
+
+
+function calAreaQuadrado (base, altura) {
+  return (base * altura);
+}
+
+function calcularTudo () {
+  switch (mode) {
+    case 'area':
+      if (formaFigura === 'quadrado') {
+        resp = calAreaQuadrado(base.value, altura.value);
+        console.log(resp);
+        resultado.value = `Área = ${resp}cm²`;
+      }
+      break
+
   }
-});
+}
